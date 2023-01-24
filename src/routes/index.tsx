fe91991c038d01c9e0ea4e966f7b395dfe36eafb
Ahmad Zain AF <zainazharul@gmail.com>
2023-01-25 02:53:18 +0700

    feat: added routes for login, register, and detail product page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,26 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import Home from "pages";
+import DetailProduct from "pages/DetailProduct";
+import Register from "pages/auth/Register";
+import Login from "pages/auth/Login";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/detailproduct",
+    element: <DetailProduct />,
   },
 ]);
 
