ac2d9014e68aeee538bd35190d5007cbb3abe24d
Ahmad Zain AF <zainazharul@gmail.com>
2023-01-25 18:43:29 +0700

    feat: added search button in navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,10 @@
-import { FaShoppingCart, FaClipboardList, FaUserCircle } from "react-icons/fa";
+import {
+  FaShoppingCart,
+  FaClipboardList,
+  FaUserCircle,
+  FaSearch,
+} from "react-icons/fa";
+import { BiSearchAlt } from "react-icons/bi";
 import { Link } from "react-router-dom";
 
 import logo from "src/assets/logo.svg";
@@ -16,21 +22,31 @@ const Navbar = () => {
         </Link>
       </section>
       <section className="navbar-center">
-        <div className="form-control hidden lg:flex">
+        <div className="form-control flex-row hidden lg:flex">
           <input
             type="text"
             placeholder="What do you want to find?"
-            className="w-80 py-2 px-3 rounded-md border-2 border-customcyan bg-gray-200"
+            className="w-60 py-2 px-3 rounded-l-lg border-y-2 border-l-2 border-customcyan bg-gray-100"
           />
+          <button className="px-2 rounded-r-lg border-y-2 border-r-2 border-customcyan bg-gray-100">
+            <div className="duration-300 hover:cursor-pointer  active:scale-75 text-gray-400">
+              <BiSearchAlt size={30} />
+            </div>
+          </button>
         </div>
       </section>
       <section className="navbar-end gap-3">
-        <div className="form-control lg:hidden">
+        <div className="form-control flex flex-row lg:hidden">
           <input
             type="text"
             placeholder="Search"
-            className="w-36 py-2 px-3 rounded-md border-2 border-customcyan bg-gray-200"
+            className="w-40 py-2 px-3 rounded-l-lg border-y-2 border-l-2 border-customcyan bg-gray-100"
           />
+          <button className="px-2 rounded-r-lg border-y-2 border-r-2 border-customcyan bg-gray-100">
+            <div className="duration-300 hover:cursor-pointer  active:scale-75 text-gray-400">
+              <BiSearchAlt size={30} />
+            </div>
+          </button>
         </div>
         <Link to="/register">
           <button className="rounded-full border-2 text-customcyan border-customcyan p-1 w-24 duration-300 hover:cursor-pointer  active:scale-90">
