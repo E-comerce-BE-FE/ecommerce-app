9ab46f9fbf3164624fc2413e3ebd3db58b3bea07
Ahmad Zain AF <zainazharul@gmail.com>
2023-01-25 14:39:41 +0700

    feat: changed card component name

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,20 @@
-import React from 'react';
+import React from "react";
 
-import Layout from 'components/Layout';
-import Navbar from 'components/Navbar';
-import ProductCard from 'components/Card';
-import 'styles/index.css';
+import Layout from "components/Layout";
+import Navbar from "components/Navbar";
+import ProductCard from "components/ProductCard";
+import "styles/index.css";
 
 function App() {
-  
-
   return (
     <Layout>
-      <div className='w-full h-screen bg-white'>
-      <Navbar />
-      <div className="w-full h-screen flex overflow-auto  bg-white">
-        <div className="w-full hp-screen">
-          <div className="flex-1 bg-white">
-            <h2
-                className='mb-10'
+      <div className="w-full h-screen bg-white">
+        <Navbar />
+        <div className="w-full h-screen flex overflow-auto  bg-white">
+          <div className="w-full hp-screen">
+            <div className="flex-1 bg-white">
+              <h2
+                className="mb-10"
                 style={{
                   fontFamily: "Poppins",
                   fontSize: "1.75em",
@@ -26,19 +24,18 @@ function App() {
                 }}
               >
                 All Product
-            </h2>
-          </div>
-          <div className="grid grid-cols-5 gap-10">
-            {[...Array(20)].map((e) => (
-                <ProductCard
-                />
+              </h2>
+            </div>
+            <div className="grid grid-cols-5 gap-10">
+              {[...Array(10)].map((e) => (
+                <ProductCard />
               ))}
+            </div>
           </div>
         </div>
       </div>
-      </div>
-    </Layout>             
-  )
+    </Layout>
+  );
 }
 
 export default App;
