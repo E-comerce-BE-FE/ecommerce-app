d5bc7ad14e05f4c72544d727d0c1afc9d947a00a
Ahmad Zain AF <zainazharul@gmail.com>
2023-01-25 15:03:57 +0700

    feat: added page navigation in navbar buttons

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -32,17 +32,27 @@ const Navbar = () => {
             className="w-36 py-2 px-3 rounded-md border-2 border-customcyan bg-gray-200"
           />
         </div>
-        <button className="rounded-full border-2 text-customcyan border-customcyan p-1 w-24 duration-300 hover:cursor-pointer  active:scale-90">
-          Signup
-        </button>
-        <button className="rounded-full border-2 text-white bg-customcyan p-1 w-24 duration-300 hover:cursor-pointer  active:scale-90">
-          Login
-        </button>
-        <div className="duration-300 hover:cursor-pointer  active:scale-90 text-customcyan hidden lg:flex">
-          <FaShoppingCart size={30} />
-        </div>
-        <div className="duration-300 hover:cursor-pointer  active:scale-90 text-customcyan hidden lg:flex">
-          <FaClipboardList size={30} />
+        <Link to="/register">
+          <button className="rounded-full border-2 text-customcyan border-customcyan p-1 w-24 duration-300 hover:cursor-pointer  active:scale-90">
+            Signup
+          </button>
+        </Link>
+        <Link to="/login">
+          <button className="rounded-full border-2 text-white bg-customcyan p-1 w-24 duration-300 hover:cursor-pointer  active:scale-90">
+            Login
+          </button>
+        </Link>
+        <div className="hidden lg:flex lg:gap-3">
+          <Link to="/mycart">
+            <div className="duration-300 hover:cursor-pointer  active:scale-90 text-customcyan">
+              <FaShoppingCart size={30} />
+            </div>
+          </Link>
+          <Link to="/transactions">
+            <div className="duration-300 hover:cursor-pointer  active:scale-90 text-customcyan">
+              <FaClipboardList size={30} />
+            </div>
+          </Link>
         </div>
         <div className="dropdown dropdown-end flex items-center">
           <label
@@ -57,15 +67,21 @@ const Navbar = () => {
             tabIndex={0}
             className="z-50 dropdown-content mt-56 p-3 bg-gray-50 rounded-box w-40 border-2 border-customcyan shadow-lg lg:mt-40"
           >
-            <li className="px-3 py-1 rounded-lg duration-300 hover:bg-gray-200 hover:cursor-pointer active:bg-customcyan active:scale-90">
-              <a>Profile</a>
-            </li>
-            <li className="px-3 py-1 rounded-lg duration-300 hover:bg-gray-200 hover:cursor-pointer active:bg-customcyan active:scale-90 lg:hidden">
-              <a>My Cart</a>
-            </li>
-            <li className="px-3 py-1 rounded-lg duration-300 hover:bg-gray-200 hover:cursor-pointer active:bg-customcyan active:scale-90 lg:hidden">
-              <a>Transactions</a>
-            </li>
+            <Link to="/profile">
+              <li className="px-3 py-1 rounded-lg duration-300 hover:bg-gray-200 hover:cursor-pointer active:bg-customcyan active:scale-90">
+                <a>Profile</a>
+              </li>
+            </Link>
+            <Link to="/mycart">
+              <li className="px-3 py-1 rounded-lg duration-300 hover:bg-gray-200 hover:cursor-pointer active:bg-customcyan active:scale-90 lg:hidden">
+                <a>My Cart</a>
+              </li>
+            </Link>
+            <Link to="/transactions">
+              <li className="px-3 py-1 rounded-lg duration-300 hover:bg-gray-200 hover:cursor-pointer active:bg-customcyan active:scale-90 lg:hidden">
+                <a>Transactions</a>
+              </li>
+            </Link>
             <li className="px-3 py-1 rounded-lg duration-300 hover:bg-gray-200 hover:cursor-pointer active:bg-customcyan active:scale-90">
               <a>Logout</a>
             </li>
