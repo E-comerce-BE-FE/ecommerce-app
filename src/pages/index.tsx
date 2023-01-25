280314929d742531367fe4a56fdd381be95c91d6
Ahmad Zain AF <zainazharul@gmail.com>
2023-01-25 23:20:55 +0700

    feat: added content horizontal padding and added button to add new product

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,5 @@
+import { BsFillPlusCircleFill } from "react-icons/bs";
+import { Link } from "react-router-dom";
 import React from "react";
 
 import Layout from "components/Layout";
@@ -8,30 +10,33 @@ import "styles/index.css";
 function App() {
   return (
     <Layout>
-      <div className="w-full h-screen bg-white">
-        <Navbar />
-        <div className="w-full h-screen flex overflow-auto  bg-white">
-          <div className="w-full hp-screen">
-            <div className="flex-1 bg-white">
-              <h2
-                className="mb-10"
-                style={{
-                  fontFamily: "Poppins",
-                  fontSize: "1.75em",
-                  fontWeight: "700",
-                  textAlign: "center",
-                  color: "#000000",
-                }}
-              >
-                All Product
-              </h2>
+      <Navbar />
+      <div className="w-full hp-screen px-20 pb-20">
+        <div className="flex-1">
+          <h2
+            className="mb-10"
+            style={{
+              fontFamily: "Poppins",
+              fontSize: "1.75em",
+              fontWeight: "700",
+              textAlign: "center",
+              color: "#000000",
+            }}
+          >
+            All Product
+          </h2>
+        </div>
+        <div className="grid grid-cols-5 gap-10">
+          {[...Array(30)].map((e) => (
+            <ProductCard />
+          ))}
+        </div>
+        <div className="sticky bottom-20 flex justify-end mr-20 text-customcyan">
+          <Link to="add-new-product">
+            <div className="bg-gray-50 rounded-full p-1 duration-300 hover:cursor-pointer hover:text-cyan-300 active:scale-90">
+              <BsFillPlusCircleFill size={50} />
             </div>
-            <div className="grid grid-cols-5 gap-10">
-              {[...Array(10)].map((e) => (
-                <ProductCard />
-              ))}
-            </div>
-          </div>
+          </Link>
         </div>
       </div>
     </Layout>
