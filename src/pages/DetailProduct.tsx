6220d629e577a5caee4d1802ef58dc033feb91c8
Ahmad Zain AF <zainazharul@gmail.com>
2023-01-27 00:58:28 +0700

    feat: added description integration and added more information on stock and price

--- src/pages/DetailProduct.tsx
+++ src/pages/DetailProduct.tsx
@@ -12,6 +12,7 @@ interface TypeProduct {
   price: number;
   product_image: string;
   user: User;
+  description: string;
 }
 
 interface User {
@@ -34,7 +35,7 @@ const DetailProduct = () => {
       .get(`/products/${id}`)
       .then((res) => {
         setProduct(res.data.data);
-        //console.log(res.data)
+        console.log(res.data);
       })
       .catch((err) => {
         alert(err());
@@ -46,15 +47,19 @@ const DetailProduct = () => {
       <Navbar />
       <section className="flex justify-center items-center gap-10 mx-40 border-2 border-customcyan p-10 rounded-t-3xl">
         <div className="border-2 border-customcyan rounded-2xl">
-          <img src={product?.product_image} alt="image" />
+          <img
+            src={product?.product_image}
+            alt="image"
+            className="w-80 h-80 m-2 rounded-2xl"
+          />
         </div>
-        <div className="flex flex-col gap-10">
-          <div className="flex items-center gap-5">
+        <div className="flex flex-col gap-16">
+          <div className="flex items-center gap-10">
             <div className="flex flex-col gap-5 w-3/5">
               <div className="font-bold text-xl">{product?.product_name}</div>
-              <div className="text-gray-500">{product?.stock}</div>
+              <div className="text-gray-500">Stock: {product?.stock}</div>
               <div className="w-fit py-2 px-5 mt-1 border-2 border-customcyan rounded-xl text-center">
-                {product?.price}
+                Rp {product?.price} ,-
               </div>
             </div>
             <div>
@@ -80,23 +85,7 @@ const DetailProduct = () => {
       </section>
       <section className="flex flex-col gap-5 mx-40 mt-5 mb-20 border-2 border-customcyan p-10 rounded-b-3xl">
         <span className="text-customcyan font-semibold">Description</span>
-        <span>
-          Model Layaknya baju kebanyakan, model pasti jadi salah satu
-          pertimbangan utama ketika seseorang memilih baju, termasuk gamis.
-          Beberapa hal yang seringkali menjadi pertimbangan adalah model kancing
-          samping, model pergelangan tangan dan model lipit di bagian badan
-          gamis. Bagaimana baju gamis jatuh di tubuh ketika dikenakan juga jadi
-          hal yang penting. Ukuran Deskripsi baju gamis yang lengkap dapat
-          meyakinkan pelanggan untuk membeli produk Anda, termasuk dengan
-          menyertakan berbagai ukuran baju yang memang sesuai antara yang Anda
-          tuliskan dan sesungguhnya. Kualtias Bahan Selain itu, bahan yang
-          nyaman, ringan, tidak mudah rusak serta menyerap keringat juga
-          biasanya menjadi pilihan banyak pembeli. Harga Setelah semuanya cocok
-          baik dari segi model, ukuran, dan kualitas bahan, akhirnya harga yang
-          menjadi penentuan. Memang harga sesuai dengan kualitas namun pastikan
-          Anda menjelaskan kenapa produk Anda memang memiliki harga yang relatif
-          lebih mahal, misalnya dari segi bahan yang langka dan berkualitas.
-        </span>
+        <span>{product?.description}</span>
       </section>
     </Layout>
   );
