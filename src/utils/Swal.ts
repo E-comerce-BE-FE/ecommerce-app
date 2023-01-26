88f429ff9cdbb7a19d5012e4de0cd0d91b6db8e1
Ahmad Zain AF <zainazharul@gmail.com>
2023-01-26 17:13:15 +0700

    feat: created swal utils

--- src/utils/Swal.ts
+++ src/utils/Swal.ts
@@ -2,7 +2,7 @@ import Swal from "sweetalert2";
 
 export default Swal.mixin({
   reverseButtons: true,
-  confirmButtonColor: "#A91079",
+  confirmButtonColor: "#22CAB6",
   showCancelButton: true,
   showClass: {
     popup: "animate__animated animate__fadeIn animate__faster",
