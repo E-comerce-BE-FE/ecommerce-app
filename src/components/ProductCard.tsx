c69e605544331a89926680eb16e70a2f4f2e82b7
Ahmad Zain AF <zainazharul@gmail.com>
2023-01-27 07:44:04 +0700

    feat: enhanced product card so it will be properly reusable

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,23 +1,50 @@
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { FC, ReactNode } from "react";
 
-const ProductCard = (props: any) => {
-  const { productData } = props;
+interface CardProps {
+  id: number;
+  image?: string;
+  name?: string;
+  address?: string;
+  price?: number;
+  onClickCart?: () => void;
+}
+
+const ProductCard: FC<CardProps> = ({
+  id,
+  image,
+  name,
+  address,
+  price,
+  onClickCart,
+}) => {
+  const navigate = useNavigate();
+
+  function onClickDetail() {
+    navigate(`/detail-product/${id}`);
+  }
 
   return (
-    <Link to={`/detail-product/${productData.id}`}>
-      <div className="card card-compact w-full h-full bg-gray-300 shadow-xl p-3 duration-300 hover:cursor-pointer hover:scale-105 active:scale-100 rounded-box">
-        <figure className="pb-3 w-full">
-          <img
-            src={productData.product_images}
-            alt="Shoes"
-            className="rounded-xl duration-300 hover:scale-105 h-60"
-          />
-        </figure>
-        <h2 className="card-title">{productData.product_name}</h2>
-        <p>{productData.address}</p>
-        <p className="font-semibold">{productData.price}</p>
-      </div>
-    </Link>
+    <div
+      className="card card-compact w-full h-full bg-customcyan shadow-xl p-3 duration-300 hover:cursor-pointer hover:scale-105 active:scale-100 rounded-box"
+      onClick={() => onClickDetail()}
+    >
+      <figure className="pb-3 w-full">
+        <img
+          src={image}
+          alt="image"
+          className="rounded-xl duration-300 bg-gray-100 hover:scale-105 h-52 w-full"
+        />
+      </figure>
+      <h2
+        className="p-2 text-customcyan rounded-lg text-lg font-extrabold bg-gray-100
+      "
+      >
+        {name}
+      </h2>
+      <p className="px-2 pt-2 pb-1">{address}</p>
+      <p className="px-2 font-semibold">{price}</p>
+    </div>
   );
 };
 
